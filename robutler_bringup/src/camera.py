9a3371e18556998418afc1316d38a555acd3300c
cmrsantos <cmrsantos@ua.pt>
2023-02-02 15:22:05 +0000

    adição de rodeio e texto na imagem do ros

--- robutler_bringup/src/camera.py
+++ robutler_bringup/src/camera.py
@@ -3,55 +3,48 @@
 
 import rospy
 from sensor_msgs.msg import Image
-
 import cv2
-from cv_bridge import CvBridge, CvBridgeError
-
 import numpy as np
+from cv_bridge import CvBridge, CvBridgeError
 
 bridge = CvBridge()
 
+def detect_purple_sphere(img):
+    # Convert the image to HSV color space
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-def imageCallback(msg):
+    # Define the range of purple color in HSV color space
+    lower_purple = np.array([120, 50, 50])
+    upper_purple = np.array([140, 255, 255])
+
+    # Threshold the image to get only purple color
+    mask = cv2.inRange(hsv, lower_purple, upper_purple)
+
+    # Find the contours in the thresholded image
+    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Draw a circle around the detected sphere
+    for contour in contours:
+        (x, y), radius = cv2.minEnclosingCircle(contour)
+        center = (int(x), int(y))
+        radius = int(radius)
+        img = cv2.circle(img, center, radius, (0, 255, 0), 2)
+
+    return img
+
+def image_callback(msg):
     # Convert image to opencv
     try:
         img = bridge.imgmsg_to_cv2(msg, "bgr8")
-    except Exception:
-        print('Failed to convert image')
+    except CvBridgeError as e:
+        print('Failed to convert image:', e)
         return
 
-    # Convert image to HSV color space
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    # Detect purple sphere
+    img = detect_purple_sphere(img)
 
-    # Define lower and upper bounds for purple color in HSV color space
-    lower_purple = np.array([125, 0, 0])
-    upper_purple = np.array([175, 255, 255])
-
-    # Threshold the image to get only the purple pixels
-    mask = cv2.inRange(hsv, lower_purple, upper_purple)
-
-    # Perform morphological operations to remove noise
-    kernel = np.ones((5,5),np.uint8)
-    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-
-    # Perform blob detection to detect the purple sphere
-    params = cv2.SimpleBlobDetector_Params()
-    params.filterByColor = True
-    params.blobColor = 255
-    detector = cv2.SimpleBlobDetector_create(params)
-    keypoints = detector.detect(mask)
-
-    # Draw a circle around the detected purple sphere
-    for kp in keypoints:
-        x, y = int(kp.pt[0]), int(kp.pt[1])
-        size = int(kp.size)
-        cv2.circle(img, (x, y), size, (0, 0, 255), 2)
-
-    # Display a text label if a purple sphere is detected
-    if len(keypoints) > 0:
-        cv2.putText(img, "Here is a purple sphere", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-
-    # Display the image
+    # Display image with message
+    cv2.putText(img, "Here's a purple sphere", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
     cv2.imshow("Robutler's Camera", img)
 
     # Keyboard inputs
@@ -60,17 +53,13 @@ def imageCallback(msg):
     if key == ord('q') or key == 27:    # Q or ESC to exit
         pass
 
-
-
 def main():
-
     # Image subscriber
-    rospy.init_node('camera')
-    rospy.Subscriber("/camera/rgb/image_raw", Image, imageCallback)
+    rospy.init_node('camera', anonymous=True)
+    rospy.Subscriber("/camera/rgb/image_raw", Image, image_callback)
 
     # Spin until ctrl+c
     rospy.spin()
-
 
 if __name__ == '__main__':
     main()
