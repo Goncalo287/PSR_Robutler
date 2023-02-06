e2041edb1900e808609f6c6015ea5d114281084c
cmrsantos <cmrsantos@ua.pt>
2023-02-06 12:35:12 +0000

    deteção de esferas

--- robutler_bringup/src/camera.py
+++ robutler_bringup/src/camera.py
@@ -10,27 +10,33 @@ from cv_bridge import CvBridge, CvBridgeError
 bridge = CvBridge()
 
 def detect_spheres(img, color):
-    print(color)
-    
     # Convert the image to HSV color space
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-    # Define the range of purple color in HSV color space
-    lower_purple = np.array([125, 50, 50])
-    upper_purple = np.array([135, 255, 255])
+    # Threshold the image to get only desired color
+    if color == "sphere_violet":
+        lower_color = np.array([125, 50, 50])
+        upper_color = np.array([140, 255, 255])
+        mask = cv2.inRange(hsv, lower_color, upper_color)
+    elif color == "sphere_red":
+        lower_color = np.array([0, 50, 50])
+        upper_color = np.array([10, 255, 255])
+        mask = cv2.inRange(hsv, lower_color, upper_color)
+    else:
+        lower_purple = np.array([125, 50, 50])
+        upper_purple = np.array([140, 255, 255])
+        lower_red = np.array([0, 50, 50])
+        upper_red = np.array([10, 255, 255])
+        mask_purple = cv2.inRange(hsv, lower_purple, upper_purple)
+        mask_red = cv2.inRange(hsv, lower_red, upper_red)
+        mask = cv2.bitwise_or(mask_purple, mask_red)
+        kernel_red = np.ones((5, 5), np.uint8)
+        mask_red = cv2.morphologyEx(mask_red, cv2.MORPH_CLOSE, kernel_red)
+        contours_red, _ = cv2.findContours(mask_red, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        
+        
 
-    # Threshold the image to get only purple color
-    mask_purple = cv2.inRange(hsv, lower_purple, upper_purple)
-
-    # Define the range of red color in HSV color space
-    lower_red = np.array([0, 50, 50])
-    upper_red = np.array([10, 255, 255])
-
-    # Threshold the image to get only red color
-    mask_red = cv2.inRange(hsv, lower_red, upper_red)
-
-    # Combine the two masks
-    mask = cv2.bitwise_or(mask_purple, mask_red)
+    
 
     # Smooth out the mask using morphological operations
     kernel = np.ones((5, 5), np.uint8)
@@ -41,6 +47,18 @@ def detect_spheres(img, color):
 
     # Draw a circle around the detected spheres
     spheres_detected = False
+    sphere_count = 0
+    red_sphere_count = 0
+    purple_sphere_count = 0
+    for contour in contours_red:
+        if cv2.contourArea(contour) < 200:
+            continue
+
+        (x, y), radius = cv2.minEnclosingCircle(contour)
+        center = (int(x), int(y))
+        radius = int(radius)
+        red_sphere_count += 1
+        
     for contour in contours:
         if cv2.contourArea(contour) < 200:
             continue
@@ -49,13 +67,32 @@ def detect_spheres(img, color):
         center = (int(x), int(y))
         radius = int(radius)
 
-        # Check if the sphere is purple or red
-        if cv2.countNonZero(mask_purple[int(y) - int(radius):int(y) + int(radius), int(x) - int(radius):int(x) + int(radius)]) > cv2.countNonZero(mask_red[int(y) - int(radius):int(y) + int(radius), int(x) - int(radius):int(x) + int(radius)]):
+        if color == "sphere_violet":
             color = (0, 255, 0)
             text = "Here's a purple sphere"
-        else:
-            color = (0, 0, 255)
+        elif color == "sphere_red":
+            color = (0, 255, 0)
             text = "Here's a red sphere"
+        else:
+            sphere_count += 1
+            purple_sphere_count = sphere_count-red_sphere_count
+            color = (255, 0, 0)
+            if sphere_count==0:
+                text = "No spheres"
+            else:
+                if red_sphere_count == 1 and purple_sphere_count == 0:
+                    text = "Here's a red sphere "
+                elif red_sphere_count == 0 and purple_sphere_count == 1:
+                    text = "Here's a purple sphere "
+                elif red_sphere_count > 0 and purple_sphere_count == 0:
+                    text = "Here are " + str(red_sphere_count) + " red spheres "
+                elif red_sphere_count == 0 and purple_sphere_count > 0:
+                    text = "Here are " + str(purple_sphere_count) + " purple spheres "
+                elif red_sphere_count == 1 and purple_sphere_count == 1:
+                    text = "Here is a red and a purple sphere "
+                else:
+                    text = "Here is " + str(red_sphere_count) + " red spheres and " + str(purple_sphere_count) + " purple spheres "
+                
 
         img = cv2.circle(img, center, radius, color, 2)
         spheres_detected = True
@@ -76,7 +113,7 @@ def image_callback(msg):
         return
 
     # Detect spheres
-    img = detect_spheres(img)
+    img, sucess = detect_spheres(img,color= None)
     cv2.imshow("Robutler's Camera", img)
 
     # Keyboard inputs
